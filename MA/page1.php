8c028e9fe5b50edd27446c7dcfd76435d974ad38
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-07 09:31:29 +0100

    Update page1.php

--- MA/page1.php
+++ MA/page1.php
@@ -7,7 +7,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-    
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Source+Code+Pro&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
     <title>Document</title>
        
 
@@ -20,7 +22,8 @@
     <div style="background-image: linear-gradient(to top, #fff0 0%, #fff 100%);padding: 100px;display: inline-block;margin-left: auto;" >
     <p style="text-align:center"> EPSILON<br>epsi lille</p>
     <p style="text-align:center"> Korentin Humetz</p>
-    <a href=page2.php ><p style="text-align:center">Upload de fichier</a>
+    <button class="btn btn-primary" onclick="window.location.href='page2.php'"><p style="text-align:center">Upload de fichier</button>
+
     <br>
     <img src="img/img.jpg" style=" display: block; margin-left: auto; margin-right: auto; width: 50%;">
 </div>
