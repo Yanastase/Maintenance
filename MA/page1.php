a5f8a2708d02c4cd85fe7b89f64bbac601abd65f
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-07 09:17:59 +0100

    Update page1.php

--- MA/page1.php
+++ MA/page1.php
@@ -6,24 +6,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Source+Code+Pro&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="index.css">
+    
     <title>Document</title>
+       
+
+        
+
    
 </head>
-<body style="background-color:rgb(47,79,79);">
-<div style="background-image: linear-gradient(to top, #fff0 0%, #fff 100%);">
-    
-    <p class="text-center"> EPSILON</p>
-    <p class="text-center"> Korentin Humetz</p>
-    <a href=page2.php><p style="text-align:center">Upload de fichier</a>
-    <img src="img/img.jpg" class="rounded mx-auto d-block">
-    
+<body  style="background-color:rgb(47,79,79);">
 
-
-
-
+    <div style="background-color: white;padding: 20px;display: inline-block;margin-left: auto;" >
+    <p style="text-align:center"> EPSILON<br>epsi lille</p>
+    <p style="text-align:center"> Korentin Humetz</p>
+    <a href=page2.php ><p style="text-align:center">Upload de fichier</a>
+    <br>
+    <img src="img/img.jpg" style=" display: block; margin-left: auto; margin-right: auto; width: 50%;">
 </div>
 
 
