5c6adddfaba3fc256533d4eb94cbcbc4a240575e
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-07 09:22:39 +0100

    Update page2.php

--- MA/page2.php
+++ MA/page2.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulaire d'Upload de Fichier</title>
 </head>
-<body>
-
+<body  style="background-color:rgb(47,79,79);">
+<div style="background-image: linear-gradient(to top, #fff0 0%, #fff 100%);padding: 100px;display: inline-block;margin-left: auto;" >
 <form action="upload.php" method="post" enctype="multipart/form-data">
     <label for="file">Sélectionnez un fichier :</label>
     <br>
@@ -15,6 +15,7 @@
     <br>
     <input type="submit" name="submit" value="Envoyer">
 </form>
+</div>
 
 </body>
 </html>
