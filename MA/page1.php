8b95744f5a29eb1bb6407ac7c2f25312b64db291
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-07 08:51:39 +0100

    Update page1.php

--- MA/page1.php
+++ MA/page1.php
@@ -17,7 +17,7 @@
     
     <p class="text-center"> plateforme de peer learning<br>epsi lille</p>
     <p class="text-center"> Korentin Humetz</p>
-    <a href=page2.php style="text-align:center">Upload de fichier</a>
+    <a href=page2.php><p style="text-align:center">Upload de fichier</a>
     <img src="img/img.jpg" class="rounded mx-auto d-block">
     
 
