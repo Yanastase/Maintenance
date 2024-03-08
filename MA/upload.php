570931dce69565923f31f71884fd1770ceba6a8a
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-08 13:42:33 +0100

    Update upload.php

--- MA/upload.php
+++ MA/upload.php
@@ -3,19 +3,19 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     if (isset($_FILES["file"]) && $_FILES["file"]["error"] == UPLOAD_ERR_OK) {
-        $targetDir = "uploads/";
+        $targetDir = "uploads/"; //variable du repertoire 
         $targetFile = $targetDir . basename($_FILES["file"]["name"]);
 
         
-        if (!file_exists($targetFile)) {
+        if (!file_exists($targetFile)) { //permetre d'eviter que le meme fichier d'etre importer deux fois
             
-            move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile);
-            echo "Le fichier a été correctement uploadé.";
+            move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile); //envoie le fichier dans le repertoire defini par $TargetFile
+            echo "Le fichier est correctement uploadé.";
         } else {
             echo "Désolé, le fichier existe déjà.";
         }
     } else {
-        echo "Une erreur est survenue lors de l'upload du fichier.";
+        echo "Une erreur est survenue.";
     }
 }
 ?>
