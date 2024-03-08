12b43b61430bc58c9317e55a57666183710615c9
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-08 13:42:58 +0100

    Update page1.php

--- MA/page1.php
+++ MA/page1.php
@@ -1,29 +1,45 @@
-<?php
 
-?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="index.css">
-    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&family=Source+Code+Pro&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
+    
     <title>Document</title>
-       
+    <style>
+        body {
+            margin: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        div {
+            background-image: linear-gradient(to top, #fff0 0%, #fff 100%);
+            padding: 100px;
+            display: inline-block;
+            text-align: center;
+        }
+
+        
+    </style>   
 
         
 
    
 </head>
 <body  style="background-color:rgb(47,79,79);">
+</html>
+<html>
+<br>
 
-    <div style="background-image: linear-gradient(to top, #fff0 0%, #fff 100%);padding: 100px;display: inline-block;margin-left: auto;" >
-    <p style="text-align:center"> EPSILON<br>epsi lille</p>
-    <p style="text-align:center"> Korentin Humetz</p>
-    <button class="btn btn-primary" onclick="window.location.href='page2.php'"><p style="text-align:center">Upload de fichier</button>
-
+    <div  >
+    <p style="text-align:center;color:red;"> EPSILON<br>epsi lille</p>
+    <p style="text-align:center;color:red;"> Korentin Humetz</p>
+    <button class="btn btn-primary" onclick="window.location.href='page2.php'"><p style="text-align:center;">Upload de fichier</button>
     <br>
     <img src="img/img.jpg" style=" display: block; margin-left: auto; margin-right: auto; width: 50%;">
 </div>
