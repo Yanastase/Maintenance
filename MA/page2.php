7277569a36a9aabc7e2d78f907779ad9dd1dd236
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-08 13:50:09 +0100

    Update page2.php

--- MA/page2.php
+++ MA/page2.php
@@ -1,21 +1,51 @@
-<?php
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    if (isset($_FILES["file"]) && $_FILES["file"]["error"] == UPLOAD_ERR_OK) {
-        $targetDir = "uploads/"; //variable du repertoire 
-        $targetFile = $targetDir . basename($_FILES["file"]["name"]);
 
-        
-        if (!file_exists($targetFile)) { //permetre d'eviter que le meme fichier d'etre importer deux fois
-            
-            move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile); //envoie le fichier dans le repertoire defini par $TargetFile
-            echo "Le fichier est correctement uploadé.";
-        } else {
-            echo "Désolé, le fichier existe déjà.";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Formulaire d'Upload de Fichier</title>
+</head>
+<style>
+body {
+            margin: 0;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
         }
-    } else {
-        echo "Une erreur est survenue.";
-    }
-}
-?>
+
+        div {
+            background-image: linear-gradient(to top, #fff0 0%, #fff 100%);
+            padding: 100px;
+            display: inline-block;
+            text-align: center;
+        }
+    </style>
+<body  style="background-color:rgb(47,79,79);">
+<div  >
+<form action="upload.php" method="post" enctype="multipart/form-data"><!--utilise upload.php afin d'envoyer le doc choisis dans le repertoire uploads-->
+    <label for="file"><p class=red>Sélectionnez un fichier :</label>
+    <br>
+    <input type="file" name="file" id="file" accept=".pdf, .jpg, .jpeg, .png, .gif"><!--formulaire type file pour permettre de choisir un doc-->
+    <br>
+    <input type="submit" name="submit" value="Envoyer"><!--envoie du document-->
+</form>
+</div>
+<script>
+        function validateForm() {
+            var fileInput = document.getElementById('file');
+            var filePath = fileInput.value;
+            var allowedExtensions = /(\.pdf|\.jpg|\.jpeg|\.png|\.gif)$/i;
+
+            if (!allowedExtensions.exec(filePath)) {
+                alert('Veuillez télécharger un fichier de type PDF, JPG, JPEG, PNG ou GIF.');
+                return false;
+            }
+            return true;
+        }
+    </script>
+</body>
+</html>
