f7ce5df88520a9b81206c2aaefc7360389885b12
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-08 13:43:29 +0100

    Update page2.php

--- MA/page2.php
+++ MA/page2.php
@@ -1,21 +1,21 @@
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulaire d'Upload de Fichier</title>
-</head>
-<body  style="background-color:rgb(47,79,79);">
-<div style="background-image: linear-gradient(to top, #fff0 0%, #fff 100%);padding: 100px;display: inline-block;margin-left: auto;" >
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    <label for="file">Sélectionnez un fichier :</label>
-    <br>
-    <input type="file" name="file" id="file">
-    <br>
-    <input type="submit" name="submit" value="Envoyer">
-</form>
-</div>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    if (isset($_FILES["file"]) && $_FILES["file"]["error"] == UPLOAD_ERR_OK) {
+        $targetDir = "uploads/"; //variable du repertoire 
+        $targetFile = $targetDir . basename($_FILES["file"]["name"]);
 
-</body>
-</html>
+        
+        if (!file_exists($targetFile)) { //permetre d'eviter que le meme fichier d'etre importer deux fois
+            
+            move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile); //envoie le fichier dans le repertoire defini par $TargetFile
+            echo "Le fichier est correctement uploadé.";
+        } else {
+            echo "Désolé, le fichier existe déjà.";
+        }
+    } else {
+        echo "Une erreur est survenue.";
+    }
+}
+?>
