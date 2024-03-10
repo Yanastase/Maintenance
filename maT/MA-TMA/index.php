7f52d31c036fb8391c63b82531187911dd9e8fba
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-10 15:34:37 +0100

    Add files via upload

---
+++ maT/MA-TMA/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Epsilon</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="card">
+  <h2>EPSILON</h2>
+  <h3>Plateforme de peer-learning</h3>
+  <h3>Epsi Lille</h3>
+  <h3>15/20</h3>
+  <img src="/img//theo.jpg">
+  <img src="/img//corentinT.jpg"/>
+  <p>Theo Sins</p>
+  <a href="form.php" id="upload">Upload</a>
+  </div>
+</body>
+</html>
