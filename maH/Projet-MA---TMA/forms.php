fa6dfa6d776505381f1789b791a0f7b5b1a8b15d
Korentin HUMETZ <56349827+Yanastase@users.noreply.github.com>
2024-03-10 15:34:56 +0100

    Add files via upload

---
+++ maH/Projet-MA---TMA/forms.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="css.css" rel="stylesheet"/>
+</head>
+<body>
+  <div class="principal-2">
+    <div class="epsilon-2">
+      <form action="" method="post" enctype="multipart/form-data">
+          <h1>Upload</h1>
+          <p>
+            <label for="monfichier">Sélectionnez un fichier : </label>
+            <input type="file" name="monfichier" id="monfichier" />
+          </p>    
+          <p>
+            <input type="submit" value="Envoyer le fichier" />
+          </p>
+      </form>
+      <a href="index.php">return</a>
+    </div>
+  </div>
+</body>
+</html>
+
+
+<?php
+$dest = "upload/";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES['monfichier'])) {
+    $filename = $_FILES['monfichier']['name'];
+    $fileExtension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+
+    // Liste des extensions autorisées
+    $allowedExtensions = array("pdf", "jpg", "jpeg", "png", "gif","docx");
+
+    if (in_array($fileExtension, $allowedExtensions)) {
+        if ($_FILES['monfichier']['error'] === UPLOAD_ERR_OK) {
+            $resultat = move_uploaded_file($_FILES['monfichier']['tmp_name'], $dest . $filename);
+            if ($resultat) {
+                echo "<p>Fichier téléchargé avec succès !</p>";
+            } else {
+                echo "<p>Erreur lors du téléchargement du fichier.</p>";
+            }
+        } else {
+            echo "<p>Erreur lors du transfert du fichier.</p>";
+        }
+    } else {
+        echo "<p>Seuls les fichiers PDF, JPG, JPEG, PNG , docx et GIF sont autorisés.</p>";
+    }
+}
+?>
+
+
+
+</body>
+</html>
+
+
+</body>
+</html>
---
+++ maH/Projet-MA---TMA/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload</title>
+    <link href="css.css" rel="StyleSheet" />
+</head>
+<body>
+    <div class="principal">
+        <div class="epsilon">
+            <h1>Epsilon</h1>
+            <p>Plateforme de peer-learning Epsi Lille</p>
+            <img src="hugo.jpg" alt="hugo travers" />
+            <img src="corentinT.jpg" alt="Corentin tolisso"/>
+            <p>Hugo Hochart</p>
+            <p>Groupe : Hochart, Théo Duvel, Korentin </p>
+            <p>16/20<p>
+            <a href="forms.php">Upload your File !</a>
+            <p>Copyright © Epsi Lille 2024</p>
+        </div>
+    </div>  
+</body>
+</html>
